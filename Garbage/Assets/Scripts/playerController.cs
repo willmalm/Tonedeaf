a818df2c4d5e6a88f4dff4bcd382549bde14abaf
MonSell <simpa201496@hotmail.com>
2016-02-01 14:50:48 +0100

    Movement Script

---
+++ Garbage/Assets/Scripts/playerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	public GameObject player;
+	private PlayerVariables playerVar;
+	private Animator animator;
+	private Rigidbody2D rb;
+	public GameObject collider;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		rb = collider.GetComponent<Rigidbody2D> ();
+		playerVar = player.GetComponent<PlayerVariables> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey ("left"))
+		{
+			rb.MovePosition(transform.position - transform.right * playerVar.speed * Time.deltaTime);
+			animator.SetInteger ("Direction", 1);
+		}
+		
+		if (Input.GetKey ("right"))
+		{
+			rb.MovePosition(transform.position + transform.right * playerVar.speed * Time.deltaTime);
+			animator.SetInteger ("Direction", 3);
+		}
+		
+		if (Input.GetKey ("up"))
+		{
+			rb.MovePosition(transform.position + transform.up * playerVar.speed * Time.deltaTime);
+			animator.SetInteger ("Direction", 2);
+		}
+		
+		if (Input.GetKey ("down"))
+		{
+			rb.MovePosition(transform.position - transform.up * playerVar.speed * Time.deltaTime);
+			animator.SetInteger ("Direction", 0);
+		}
+	}
+
+
+	void FixedUpdate()
+	{
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+
+		/*if (vertical < 0) 
+		{
+			animator.SetInteger ("Direction", 1);
+		} 
+		else if (vertical > 0)
+		{
+			animator.SetInteger ("Direction", 3);
+		}
+		else if (horizontal < 0)
+		{
+			animator.SetInteger ("Direction", 0);
+		} 
+		else if (horizontal > 0)
+		{
+			animator.SetInteger	("Direction", 2);
+		}*/
+	}
+}
