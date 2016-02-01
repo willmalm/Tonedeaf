c3ff79a0245bf8c153e7f4c450f324e43bb4fa5f
HoatziBot <Treezzy@hotmail.com>
2016-02-01 15:27:04 +0100

    idunno

--- Garbage/Assets/Scripts/playerController.cs
+++ Garbage/Assets/Scripts/playerController.cs
@@ -2,22 +2,20 @@
 using System.Collections;
 
 public class playerController : MonoBehaviour {
-	public GameObject player;
+
+    public GameObject playerSprite;
 	private PlayerVariables playerVar;
 	private Animator animator;
 	private Rigidbody2D rb;
-	public GameObject collider;
 
-	// Use this for initialization
 	void Start ()
 	{
-		animator = GetComponent<Animator> ();
-		rb = collider.GetComponent<Rigidbody2D> ();
-		playerVar = player.GetComponent<PlayerVariables> ();
+		animator = playerSprite.GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
+		playerVar = GetComponent<PlayerVariables> ();
 
 	}
 	
-	// Update is called once per frame
 	void Update ()
 	{
 		if (Input.GetKey ("left"))
@@ -43,29 +41,5 @@ public class playerController : MonoBehaviour {
 			rb.MovePosition(transform.position - transform.up * playerVar.speed * Time.deltaTime);
 			animator.SetInteger ("Direction", 0);
 		}
-	}
-
-
-	void FixedUpdate()
-	{
-		float vertical = Input.GetAxis("Vertical");
-		float horizontal = Input.GetAxis("Horizontal");
-
-		/*if (vertical < 0) 
-		{
-			animator.SetInteger ("Direction", 1);
-		} 
-		else if (vertical > 0)
-		{
-			animator.SetInteger ("Direction", 3);
-		}
-		else if (horizontal < 0)
-		{
-			animator.SetInteger ("Direction", 0);
-		} 
-		else if (horizontal > 0)
-		{
-			animator.SetInteger	("Direction", 2);
-		}*/
 	}
 }
