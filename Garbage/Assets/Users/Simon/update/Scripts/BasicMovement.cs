fde9f1f3ba8c70b03d9005763baa57f00078f973
MonSell <simpa201496@hotmail.com>
2016-02-03 15:33:08 +0100

    Update

---
+++ Garbage/Assets/Users/Simon/update/Scripts/BasicMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovement : MonoBehaviour {
+
+    public float speed;
+    public float jump;
+
+    Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("left"))
+        {
+            rb2D.MovePosition(transform.position - transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            rb2D.MovePosition(transform.position + transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            rb2D.MovePosition(transform.position + transform.up * jump * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Garbage/Assets/Users/Simon/update/Scripts/Interact.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+
+    public GameObject textBox;
+    public int distance;
+
+
+    ObjectVariables objectVar;
+    Text txt;
+    RaycastHit2D hit;
+
+
+    void Start () {
+        txt = textBox.GetComponent<Text>();
+        objectVar = GetComponent<ObjectVariables>();
+	}
+
+	void Update () {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(distance, 0), Vector2.right, distance);
+
+        Debug.DrawLine(transform.position - new Vector3(distance, 0, 0), transform.position + new Vector3(distance, 0, 0));
+
+        if (hit.collider.tag == "Player")
+        {
+            txt.text = "Pick up " + objectVar.objName;
+
+            if (Input.GetKey("p"))
+            {
+                Debug.Log("Picked up " + objectVar.objName);
+            }
+        }
+        else
+        {
+            txt.text = "";
+        }
+	}
+}
+
