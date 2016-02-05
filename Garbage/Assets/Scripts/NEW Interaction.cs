9f403a7240c6baadf062812b1b666116864a4a93
MonSell <simpa201496@hotmail.com>
2016-02-05 10:32:42 +0100

    Obj. Interaction

---
+++ Garbage/Assets/Scripts/NEW Interaction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Interaction : MonoBehaviour {
+
+    public GameObject textBox;
+    public int distance;
+
+
+    ObjectVariables objectVar;
+    Text txt;
+    RaycastHit2D hit;
+
+
+    void Start()
+    {
+        txt = textBox.GetComponent<Text>();
+        objectVar = GetComponent<ObjectVariables>();
+    }
+
+    void Update() { }
+
+  
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "plInteract")
+        {
+            txt.text = "";
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "plInteract")
+        {
+            txt.text = "Pick up " + objectVar.objName;
+        }
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "plInteract")
+        {
+            if (Input.GetKeyDown("p"))
+            {
+                Debug.Log("Picked up " + objectVar.objName);
+            }
+        }
+    }
+}
