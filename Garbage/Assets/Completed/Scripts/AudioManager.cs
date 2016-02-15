1ed3c1bc69a372fa54e3e1bce613194d54207c68
HoatziBot <Treezzy@hotmail.com>
2016-02-15 14:22:06 +0100

    Update (sound)

---
+++ Garbage/Assets/Completed/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    AudioSource audioSource;
+
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    public void playSound(string file)
+    {
+        audioSource.clip = Resources.Load(file) as AudioClip;
+        audioSource.Play();
+    }
+}
--- Garbage/Assets/Completed/Scripts/PlayerController.cs
+++ Garbage/Assets/Completed/Scripts/PlayerController.cs
@@ -14,14 +14,18 @@ public class PlayerController : MonoBehaviour {
     public bool movingDown;
     public bool moving;
     public float idleTimer;
+    public float stepTimer;
+    public float stepDelay;
 
     //Scripts
     private PlayerVariables playerVar;
     private GridVariables gridVar;
     private SpriteSize playerSize;
     private JensAnimationController playerAnimation;
+    private AudioManager audioManager;
 
     private int backUpLayer;
+    private int timerSpeed;
 
 	void Start ()
 	{
@@ -33,21 +37,31 @@ public class PlayerController : MonoBehaviour {
         playerAnimation = playerSprite.GetComponent<JensAnimationController>();
         playerSize = playerSprite.GetComponent<SpriteSize>();
 
+        audioManager = GetComponent<AudioManager>();
         backUpLayer = 0;
+        timerSpeed = 0;
 	}
 
     void Update()
-    { }
+    {
+        stepTimer += timerSpeed * Time.deltaTime;
+    }
         
     public void MoveRight()
     {
         if (playerVar.immobile == false)
         {
+            timerSpeed = 1;
             idleTimer = 0;
             transform.position += new Vector3(playerVar.horizontal_speed * Time.deltaTime, 0, 0);
             playerSize.direction = -1;
             playerAnimation.speed = new Vector2(1, 0);
             playerAnimation.idleAnimation = 0;
+            if (stepTimer >= stepDelay)
+            {
+                audioManager.playSound("Hit");
+                stepTimer = 0;
+            }
         }
        
     }
@@ -55,40 +69,58 @@ public class PlayerController : MonoBehaviour {
     {
         if (playerVar.immobile == false)
         {
+            timerSpeed = 1;
             idleTimer = 0;
             transform.position += new Vector3(-playerVar.horizontal_speed * Time.deltaTime, 0, 0);
             playerSize.direction = 1;
             playerAnimation.speed = new Vector2(1, 0);
             playerAnimation.idleAnimation = 0;
+            if (stepTimer >= stepDelay)
+            {
+                audioManager.playSound("Hit");
+                stepTimer = 0;
+            }
         }
     }
     public void MoveUp()
     {
         if (playerVar.immobile == false)
         {
+            timerSpeed = 1;
             idleTimer = 0;
             transform.position += new Vector3(0, playerVar.vertical_speed * Time.deltaTime, 0);
             playerAnimation.speed = new Vector2(1, 0);
             playerAnimation.idleAnimation = 0;
+            if (stepTimer >= stepDelay)
+            {
+                audioManager.playSound("Hit");
+                stepTimer = 0;
+            }
         }
 
 
     }
     public void MoveDown()
     {
+        timerSpeed = 1;
         if (playerVar.immobile == false)
         {
             idleTimer = 0;
             transform.position += new Vector3(0, -playerVar.vertical_speed * Time.deltaTime, 0);
             playerAnimation.speed = new Vector2(1, 0);
             playerAnimation.idleAnimation = 0;
+            if (stepTimer >= stepDelay)
+            {
+                audioManager.playSound("Hit");
+                stepTimer = 0;
+            }
         }
     }
     public void Idle()
     {
             //Increase timer based on framrate
             idleTimer += Time.deltaTime;
-
+            timerSpeed = 0;
             playerAnimation.speed = new Vector2(0, 0);
             if (idleTimer >= idle1_Delay && idleTimer < idle2_Delay)
             {
