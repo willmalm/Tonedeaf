dbb192694dcbfd076b07831f54ea21ff157f2161
C7K <a15hangl@HS-S-E01219.hs.local>
2016-02-22 15:15:04 +0100

    The scenechanger is back! Tack Aili! :)

---
+++ Garbage/Assets/Completed/Scripts/SceneChangeTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneChangeTrigger : MonoBehaviour
+{
+    public string sceneName;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "plInteract")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
