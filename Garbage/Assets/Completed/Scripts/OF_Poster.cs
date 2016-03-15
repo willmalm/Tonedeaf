793b32fb80c7f9659f5037d702c426383dde9f7c
C7K <a15hangl@hs.local>
2016-03-15 13:31:43 +0100

    Update
    plz (update needed on scenes excluding Testing)

---
+++ Garbage/Assets/Completed/Scripts/OF_Poster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OF_Poster : MonoBehaviour {
+
+    ObjectVariables var;
+    SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+        var = GetComponent<ObjectVariables>();
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (var.used)
+        {
+            sprite.color -= new Color(0, 0, 0, Time.deltaTime);
+        }
+	}
+}
---
+++ Garbage/Assets/Completed/Scripts/OF_Sign.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OF_Sign : MonoBehaviour {
+    ObjectVariables var;
+	// Use this for initialization
+	void Start () {
+        var = GetComponent<ObjectVariables>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (var.used)
+        {
+            transform.eulerAngles += new Vector3(0, 0, Time.deltaTime);
+            
+        }
+	}
+}
