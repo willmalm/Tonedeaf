c6c02737ef8378a30e325ad906ae9fcf85b963a5
Flygluffet <a15danai@hs.local>
2016-03-21 17:44:11 +0100

    Added easter eggs and more objects.
    added easter egges and also reworked how the owl works. Added the
    scenechanger, radio and trash on the ground.

--- Garbage/Assets/Completed/Scripts/SpriteTransision.cs
+++ Garbage/Assets/Completed/Scripts/SpriteTransision.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpriteTransision : MonoBehaviour {
-public Texture2D tex;
-	AudioSource aSrc;
-Material m;
-ObjectVariables oV;
-bool playSound = true;
+public class SpriteTransision : MonoBehaviour
+{
 
+public Texture2D tex;
+private	AudioSource aSrc;
+private Material m;
+private ObjectVariables oV;
+private bool playSound = true;
+private bool timerStart = false;
+private float timer;
+    
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		MeshRenderer r = gameObject.GetComponent<MeshRenderer>();
 		if(r != null)
 		{
@@ -22,30 +27,39 @@ bool playSound = true;
 
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (timerStart)
+        {
+            timer += Time.deltaTime;
+            if (timer > 1f)
+            {
+                //Byt texture
+                m.SetTexture("_MainTex", tex);
+                m.SetTexture("_SecondTex", tex);
+                m.SetFloat("_Lerp", 0);
+                m.color = Color.white;
+            }
+        }
 	if (m != null && oV != null)
 	{
 		float lerp = oV.force/oV.toughness;
 		
 		
-		if(oV.used){
-			if(playSound){
-				playSound = false;
+		if(oV.used)
+        {
+		    if(playSound)
+            {
+			playSound = false;
 				
-				//SPELA LJUD KOD
-					aSrc.Play();
-				
-				
-				//Byt texture
-				m.SetTexture("_MainTex",tex);
-				m.SetTexture("_SecondTex",tex);
-				m.SetFloat("_Lerp", 0);
-				m.color = Color.white;
-			}
-			
+			//SPELA LJUD KOD
+		    aSrc.Play();
+            timerStart = true;
+		}
 			
 		}
-		else{
+		else
+        {
 			m.SetFloat("_Lerp", lerp);
 			oV.force -= Time.deltaTime*0.1f;
 			if(oV.force < 0) oV.force = 0;
